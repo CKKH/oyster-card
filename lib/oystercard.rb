e3a072ae4cc338874d9dc5833575c84904542ae3
CKKH <chriskkhassan@gmail.com>
2018-12-03 15:18:15 +0000

    step 4 - initialized Oystercard with default balance

---
+++ lib/oystercard.rb
@@ -0,0 +1,10 @@
+class Oystercard
+
+attr_accessor :balance
+DEFAULT_BALANCE = 0
+
+def initialize
+  @balance = DEFAULT_BALANCE
+end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  subject(:card) { Oystercard.new }
+
+  it 'with new card' do
+    expect(card).to respond_to(:balance)
+  end
+  it 'with new card' do
+    expect(card.balance).to eq(0)
+  end
+
 
 end
